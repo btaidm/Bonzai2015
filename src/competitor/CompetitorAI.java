73d3928b2dd135d8a57e384c816a7ef4e38726e4
Tim Bradt <tjbradt@mtu.edu>
2015-04-11 10:37:23 -0400

    Simple AI

--- src/competitor/CompetitorAI.java
+++ src/competitor/CompetitorAI.java
@@ -1,18 +1,67 @@
 package competitor;
 
-import snowbound.api.*;
+import static snowbound.api.util.Utility.any;
+import static snowbound.api.util.Utility.difference;
+import static snowbound.api.util.Utility.min;
 
-@Agent(name = "AIName")
+import java.util.HashMap;
+import java.util.Map;
+
+import snowbound.api.*;
+import snowbound.api.util.ManhattanDistance;
+
+@Agent(name = "Headcrabs")
 public class CompetitorAI extends AI{
 	
+	private Map<Unit, Base> goals;
+	
 	public CompetitorAI(){
-		
+		this.goals = new HashMap<>();
 	}
 	
 	/**
 	 *
 	 **/
 	public Action action(Turn turn) {
+		Unit actor = turn.actor();
+		
+		// spawn if you need to (who cares if there is someone already there)
+		if(actor.position() == null) { 
+			return new SpawnAction(any(turn.actor().team().spawns()), Perk.NONE);
+		}
+		
+		// if we have a goal and its satisfied, remove it
+		if(goals.containsKey(actor) && actor.team().equals(turn.current(goals.get(actor)).owner())) {
+			goals.remove(actor);
+		}
+		
+		// if we don't have a goal yet, choose one
+		if(!goals.containsKey(actor)) {
+			Base choice = any(difference(turn.allBases(), turn.myBases()));
+			if(choice != null) { goals.put(actor, choice); }
+		}
+		
+		if(actor.snowballs() < actor.statistic(Stat.CAPACITY) && Math.random() < 0.25) {
+			System.out.println("Gathering Snow! (have " + actor.snowballs() + " now)");
+			return new GatherAction();
+		}
+		
+		// respond reactively, if needed
+		Unit tango = any(actor.enemyUnitsInThrowRange());
+		if(tango != null) {
+			System.out.printf("Throwing at %s with %d snowballs!%n", tango, actor.snowballs());
+			return new ThrowAction(tango.position());
+		}
+		
+		// act out that goal
+		Base goal = goals.get(actor);
+		if(goal != null) {
+			if(goal.position().equals(actor.position())) { return new CaptureAction(); }
+		
+			Position target = min(actor.positionsInMoveRange(), new ManhattanDistance(goal));
+			return new MoveAction(target);
+		}
+		
 		return null;
 	}
 
